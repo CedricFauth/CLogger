b0a594f09c8377777f82daa9bb3faf308b9bbbdc
Cedric Fauth <cedricfauth@yahoo.de>
2019-09-26 02:31:51 +0200

    ansi codes

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define ANSI_RED_BOLD "\033[31m\033[1m"
+#define ANSI_RESET "\033[0m"
 
 int main(){
 
     printf("Hello World\n");
-    
+    printf(ANSI_RED_BOLD "This text is RED!" ANSI_RESET "\n");
+
     return 0;
 }
