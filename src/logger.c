3dcf90ea16d58d164cd766c88b6a804d7f3047ea
Cedric Fauth <cedricfauth@yahoo.de>
2019-10-29 21:44:05 +0100

    bug fix

--- src/logger.c
+++ src/logger.c
@@ -6,27 +6,27 @@
 #define RED "\033[31m\033[1m"
 #define RESET "\033[0m"
 
-static LogLevel LEVEL = ERROR;
+static LogLevel LEVEL = INFO;
 
 void set_log_level(LogLevel new_level){
     LEVEL = new_level;
 }
 
 void log_info(char* msg){
-    if(INFO >= LEVEL) printf("%s[INFO]%s %s\n", BOLD, RESET, msg);
+    if(LEVEL <= INFO) printf("%s[INFO]%s %s\n", BOLD, RESET, msg);
 }
 
 void log_warn(char* msg){
-    if(INFO >= LEVEL) printf("%s[WARNING]%s %s\n",ORANGE, RESET,msg);
+    if(LEVEL <= WARNING) printf("%s[WARNING]%s %s\n",ORANGE, RESET,msg);
 }
 
 void log_err(char* msg){
-    if(INFO >= LEVEL) printf("%s[ERROR] %s%s%s%s\n",RED, RESET, BOLD, msg, RESET);
+    if(LEVEL <= ERROR) printf("%s[ERROR] %s%s%s%s",RED, RESET, BOLD, msg, RESET);
 }
 
 void log_err_detail(char* msg, char* detail, int pos){
 
-    if(INFO >= LEVEL){
+    if(LEVEL <= ERROR){
         log_err(msg);
         for(int i = 0; i < pos + 8; i++){printf(" ");}
         printf("%s^---%s%s\n", RED, detail, RESET);
